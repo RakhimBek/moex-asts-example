c0a9acb84c62438079485253620ef1af9654d4fd
Raimbek <rrakhimbekov@gmail.com>
2021-10-28 23:17:46 +0600

    Native libs description

--- src/main/java/example/com/NativeLibLoader.java
+++ src/main/java/example/com/NativeLibLoader.java
@@ -1,5 +1,8 @@
 package example.com;
 
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,7 +12,9 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import static java.nio.file.Files.createTempDirectory;
 
@@ -30,6 +35,16 @@ public final class NativeLibLoader {
 			return NativeLibLoader.class.getClassLoader();
 		}
 		return AccessController.doPrivileged((PrivilegedAction<ClassLoader>) NativeLibLoader.class::getClassLoader);
+	}
+
+	private LibsDescription readDescription() throws IOException {
+		final Constructor constructor = new Constructor(LibsDescription.class);
+		final Yaml yaml = new Yaml(constructor);
+		final ClassLoader classLoader = getClassLoader();
+
+		try (final InputStream inputStream = classLoader.getResourceAsStream("natives/libs.yaml");) {
+			return (LibsDescription) yaml.load(inputStream);
+		}
 	}
 
 	public void loadFolder(String libFolder, String libName) throws IOException {
@@ -76,5 +91,70 @@ public final class NativeLibLoader {
 		final String suffix = osarch.contains("64") ? "64" : "32";
 		System.out.println(libFolder + folder + suffix);
 		return libFolder + folder + suffix + "/";
+	}
+
+	private enum Mode {
+		EMBEDDED,
+		MTESRL,
+	}
+
+	public static class LibsDescription {
+		private Map<Mode, PlatformLibs> modes;
+		private PlatformLibs mtejni;
+
+		public Map<Mode, PlatformLibs> getModes() {
+			return modes;
+		}
+
+		public void setModes(final Map<Mode, PlatformLibs> modes) {
+			this.modes = modes;
+		}
+
+		public PlatformLibs getMtejni() {
+			return mtejni;
+		}
+
+		public void setMtejni(final PlatformLibs mtejni) {
+			this.mtejni = mtejni;
+		}
+	}
+
+	public static class PlatformLibs {
+		private List<String> linux32;
+		private List<String> linux64;
+		private List<String> windows32;
+		private List<String> windows64;
+
+		public List<String> getLinux32() {
+			return linux32;
+		}
+
+		public void setLinux32(final List<String> linux32) {
+			this.linux32 = linux32;
+		}
+
+		public List<String> getLinux64() {
+			return linux64;
+		}
+
+		public void setLinux64(final List<String> linux64) {
+			this.linux64 = linux64;
+		}
+
+		public List<String> getWindows32() {
+			return windows32;
+		}
+
+		public void setWindows32(final List<String> windows32) {
+			this.windows32 = windows32;
+		}
+
+		public List<String> getWindows64() {
+			return windows64;
+		}
+
+		public void setWindows64(final List<String> windows64) {
+			this.windows64 = windows64;
+		}
 	}
 }
